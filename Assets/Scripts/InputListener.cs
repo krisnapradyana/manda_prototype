845b33fef059d43f8beefc146a5f39abdd545827
Krisna Pradyana <krisna.superpixel.id@gmail.com>
2022-11-28 16:03:29 +0700

    Move by click functionality
    + Ray to hit point move position

--- Assets/Scripts/InputListener.cs
+++ Assets/Scripts/InputListener.cs
@@ -8,40 +8,55 @@ public class InputListener : MonoBehaviour
 {
     [SerializeField] GameHandler _gameHandler;
     [SerializeField] InputAction _moveAction;
-    [SerializeField] InputAction _pointerPos;
+    [SerializeField] InputAction _pointerInput;
+    [SerializeField] GameObject _testCursor;
+
+    Camera _cam;
+    Vector2 _mousePos = new Vector2();
+
+    int layerMask = 1 << 8;
+    
 
     private void Awake()
     {
-        _moveAction.Enable();
-        _pointerPos.Enable();
+      _moveAction.Enable();
     }
 
     private void Start()
     {
+        layerMask = ~layerMask;
+
+        _cam = Camera.main;
         _moveAction.performed += context =>
         {
-            var position = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            position.z = Camera.main.nearClipPlane;
-            MoveByMouse(position);
-            //Debug.LogFormat("Mouse Position : x{0}, y{1} ", position.x, position.y);
+            MoveByMouse(GetWorldPoint());
         };
     }
 
     private void Update()
     {
-        var position = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        position.z = Camera.main.nearClipPlane;
-        Debug.LogFormat("Mouse Position : x{0}, y{1} ", position.x, position.y, position.z);
+        //GetWorldPoint();
+        Ray ray = _cam.ScreenPointToRay(Mouse.current.position.ReadValue());
+        Debug.DrawRay(ray.origin, ray.direction * 100, Color.yellow);
     }
 
-    public void MoveByMouse(Vector2 targetPosition)
+    public void MoveByMouse(Vector3 targetPosition)
     {
-        //_gameHandler._controlledPlayer._pathFinder.Move(new Vector3(targetPosition.x, _gameHandler._controlledPlayer.transform.position.y, targetPosition.y));
+        _testCursor.transform.position = new Vector3(targetPosition.x, 5, targetPosition.z);
         _gameHandler._controlledPlayer._seekerObject.StartPath(_gameHandler._controlledPlayer.transform.position, targetPosition);
     }
 
-    public void MoveByKey()
+    private Vector3 GetWorldPoint()
     {
-        
+        Ray ray = _cam.ScreenPointToRay(Mouse.current.position.ReadValue());
+        RaycastHit hit;
+
+        Vector3 point = Vector3.zero;
+        if(Physics.Raycast(ray, out hit, Mathf.Infinity))
+        {
+            Debug.LogFormat("Mouse Position : x{0}, y{1}, z{2} ", hit.point.normalized.x, hit.point.normalized.y, hit.point.normalized.z);
+            point = new Vector3(hit.point.x, hit.point.y, hit.point.z);
+        }
+        return point;
     }
 }
