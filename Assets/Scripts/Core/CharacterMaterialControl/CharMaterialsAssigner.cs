6adbc110a5ed081a0bf4972938b964d9ed369e77
Krisna Pradyana <krisna.superpixel.id@gmail.com>
2023-01-18 11:30:23 +0700

    Character material assigner Start color
    Material assigner will use last material color if not applied at start of runtime

--- Assets/Scripts/Core/CharacterMaterialControl/CharMaterialsAssigner.cs
+++ Assets/Scripts/Core/CharacterMaterialControl/CharMaterialsAssigner.cs
@@ -32,20 +32,30 @@ public class CharMaterialsAssigner : MonoBehaviour
         var materialPack = _renderer.materials;
         for (int j = 0; j < _hairMaterialsIndex.Length; j++)
         {
+            if (!isAssignedOnStart)
+            {
+                _centralSystem.selectedHairMaterial.color = _renderer.materials[_hairMaterialsIndex[j]].color;
+            }
             materialPack[_hairMaterialsIndex[j]] = _centralSystem.selectedHairMaterial;
         }
 
         for (int j = 0; j < _eyeMaterialsIndex.Length; j++)
         {
+            if (!isAssignedOnStart)
+            {
+                _centralSystem.selectedEyeMaterial.color = _renderer.materials[_eyeMaterialsIndex[j]].color;
+            }
             materialPack[_eyeMaterialsIndex[j]] = _centralSystem.selectedEyeMaterial;
         }
 
         for (int j = 0; j < _clothesMaterialsIndex.Length; j++)
         {
+            if (!isAssignedOnStart)
+            {
+                _centralSystem.selectedClothesMaterial.color = _renderer.materials[_clothesMaterialsIndex[j]].color;
+            }
             materialPack[_clothesMaterialsIndex[j]] = _centralSystem.selectedClothesMaterial;
         }
-
-
         _renderer.materials = materialPack;
     }
 }
